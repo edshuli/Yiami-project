537f7bdaa56e53639a7dcdb5511d101c82986876
edshuli <edna_shuli@hotmail.gr>
2020-03-31 20:56:07 +0200

    Add images on API

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,22 +1,26 @@
 <template>
-<div class="container cardContainer">
-        <div classs="card">
-         <div v-for="post in posts" :key="post.id" class="card">
-             <div v-for="item in post" :key="item.idMeal">
-            <img class="card-img-top">
-            <div class="card-body">
-                <h5 class="card-title">
-                    {{item.strMeal}}
-                </h5>
-                <p class="card-text">{{item.strArea}}</p>
-                <div class="card-action text-center">
-                   <a href="#">Read More</a>
+    <div class="container cardContainer">
+        <div class="row">
+            <div class="col">
+                <div v-for="post in posts" :key="post.id" class="card">
+                    <div v-for="item in post" :key="item.idMeal">
+                        <div classs="card text-center">
+                            <img class="card-img-top" :src="item.strMealThumb"/>
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    {{item.strMeal}}
+                                </h5>
+                                <p class="card-text">{{item.strArea}}</p>
+                                <div class="card-action text-center">
+                                    <a href="#">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-         </div>
-         </div>
-    </div> 
-</div>
+        </div>
+    </div>
 </template>
 
 <script>
