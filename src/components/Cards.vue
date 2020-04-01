f5bba60aab62a9d9b2fc6166463fcea6c61121e6
edshuli <edna_shuli@hotmail.gr>
2020-04-01 15:31:10 +0200

    Fixed style on the card

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,26 +1,21 @@
 <template>
     <div class="container cardContainer">
-        <div class="row">
-            <div class="col">
-                <div v-for="post in posts" :key="post.id" class="card">
-                    <div v-for="item in post" :key="item.idMeal">
-                        <div classs="card text-center">
-                            <img class="card-img-top" :src="item.strMealThumb"/>
-                            <div class="card-body">
-                                <h5 class="card-title">
-                                    {{item.strMeal}}
-                                </h5>
-                                <p class="card-text">{{item.strArea}}</p>
-                                <div class="card-action text-center">
+          <div v-for="post in posts" :key="post.id"  class="row">
+             <div v-for="item in post" :key="item.idMeal" class="col-md-6 col-lg-6">
+                 <div class="card">
+                        <a href="">
+                            <img class="card-img-top" :src="item.strMealThumb"/></a>
+                        <div class="card-body">
+                            <h5 class="card-title">{{item.strMeal}}</h5>
+                            <p class="card-text">{{item.strArea}}</p>
+                            <div class="card-action text-center">
                                     <a href="#">Read More</a>
-                                </div>
-                            </div>
+                             </div>
                         </div>
-                    </div>
+                  </div>      
                 </div>
-            </div>
+              </div>  
         </div>
-    </div>
 </template>
 
 <script>
@@ -31,5 +26,13 @@ export default {
 </script>
 
 <style scoped>
+
+/*------Media Queries-----------*/
+
+@media(min-width: 599px){
+    .card{
+        flex-direction:inherit;
+    }
+}
 
 </style>
